8e2484bb2a93ee488d8c87be62b9ce9b1428bfe0
Intuit Service <opensource-svc@intuit.com>
2024-07-25 19:03:36 -0700

    Version 0.7.5--canary.449.15792

--- plugins/types-provider/ios/Resources/dist/TypesProviderPlugin.native.js
+++ plugins/types-provider/ios/Resources/dist/TypesProviderPlugin.native.js
@@ -1,59 +1,158 @@
 "use strict";
-var TypesProviderPlugin = (() => {
-  var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
-  var __export = (target, all) => {
-    for (var name in all)
-      __defProp(target, name, { get: all[name], enumerable: true });
-  };
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+function _array_like_to_array(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+    for(var i = 0, arr2 = new Array(len); i < len; i++)arr2[i] = arr[i];
+    return arr2;
+}
+function _array_with_holes(arr) {
+    if (Array.isArray(arr)) return arr;
+}
+function _class_call_check(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
     }
-    return to;
-  };
-  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-  // ../../../../../../../../../../../execroot/_main/bazel-out/darwin_arm64-fastbuild/bin/plugins/types-provider/core/src/index.ts
-  var src_exports = {};
-  __export(src_exports, {
-    TypesProviderPlugin: () => TypesProviderPlugin
-  });
-  var TypesProviderPlugin = class {
-    constructor(config) {
-      this.name = "TypesProviderPlugin";
-      this.config = config;
+}
+function _defineProperties(target, props) {
+    for(var i = 0; i < props.length; i++){
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
     }
-    apply(player) {
-      player.hooks.schema.tap(this.name, (schema) => {
-        if (this.config.types) {
-          schema.addDataTypes(this.config.types);
+}
+function _create_class(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+}
+function _iterable_to_array_limit(arr, i) {
+    var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"];
+    if (_i == null) return;
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _s, _e;
+    try {
+        for(_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true){
+            _arr.push(_s.value);
+            if (i && _arr.length === i) break;
         }
-        if (this.config.formats) {
-          schema.addFormatters(this.config.formats);
+    } catch (err) {
+        _d = true;
+        _e = err;
+    } finally{
+        try {
+            if (!_n && _i["return"] != null) _i["return"]();
+        } finally{
+            if (_d) throw _e;
         }
-      });
-      if (this.config.validators) {
-        player.hooks.validationController.tap(
-          this.name,
-          (validationController) => {
-            validationController.hooks.createValidatorRegistry.tap(
-              this.name,
-              (validationRegistry) => {
-                this.config.validators?.forEach(([name, handler]) => {
-                  validationRegistry.register(name, handler);
-                });
-              }
-            );
-          }
-        );
-      }
     }
-  };
-  return __toCommonJS(src_exports);
-})();
+    return _arr;
+}
+function _non_iterable_rest() {
+    throw new TypeError("Invalid attempt to destructure non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _sliced_to_array(arr, i) {
+    return _array_with_holes(arr) || _iterable_to_array_limit(arr, i) || _unsupported_iterable_to_array(arr, i) || _non_iterable_rest();
+}
+function _unsupported_iterable_to_array(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _array_like_to_array(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(n);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _array_like_to_array(o, minLen);
+}
+var TypesProviderPlugin = function() {
+    var __defProp = Object.defineProperty;
+    var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+    var __getOwnPropNames = Object.getOwnPropertyNames;
+    var __hasOwnProp = Object.prototype.hasOwnProperty;
+    var __export = function(target, all) {
+        for(var name in all)__defProp(target, name, {
+            get: all[name],
+            enumerable: true
+        });
+    };
+    var __copyProps = function(to, from, except, desc) {
+        if (from && typeof from === "object" || typeof from === "function") {
+            var _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
+            try {
+                var _loop = function() {
+                    var key = _step.value;
+                    if (!__hasOwnProp.call(to, key) && key !== except) __defProp(to, key, {
+                        get: function() {
+                            return from[key];
+                        },
+                        enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable
+                    });
+                };
+                for(var _iterator = __getOwnPropNames(from)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true)_loop();
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally{
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return != null) {
+                        _iterator.return();
+                    }
+                } finally{
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+        }
+        return to;
+    };
+    var __toCommonJS = function(mod) {
+        return __copyProps(__defProp({}, "__esModule", {
+            value: true
+        }), mod);
+    };
+    // ../../../../../../../../../../../execroot/_main/bazel-out/k8-fastbuild/bin/plugins/types-provider/core/src/index.ts
+    var src_exports = {};
+    __export(src_exports, {
+        TypesProviderPlugin: function() {
+            return TypesProviderPlugin;
+        }
+    });
+    var TypesProviderPlugin = /*#__PURE__*/ function() {
+        function TypesProviderPlugin(config) {
+            _class_call_check(this, TypesProviderPlugin);
+            this.name = "TypesProviderPlugin";
+            this.config = config;
+        }
+        _create_class(TypesProviderPlugin, [
+            {
+                key: "apply",
+                value: function apply(player) {
+                    var _this = this;
+                    player.hooks.schema.tap(this.name, function(schema) {
+                        if (_this.config.types) {
+                            schema.addDataTypes(_this.config.types);
+                        }
+                        if (_this.config.formats) {
+                            schema.addFormatters(_this.config.formats);
+                        }
+                    });
+                    if (this.config.validators) {
+                        player.hooks.validationController.tap(this.name, function(validationController) {
+                            validationController.hooks.createValidatorRegistry.tap(_this.name, function(validationRegistry) {
+                                var _this_config_validators;
+                                (_this_config_validators = _this.config.validators) === null || _this_config_validators === void 0 ? void 0 : _this_config_validators.forEach(function(param) {
+                                    var _param = _sliced_to_array(param, 2), name = _param[0], handler = _param[1];
+                                    validationRegistry.register(name, handler);
+                                });
+                            });
+                        });
+                    }
+                }
+            }
+        ]);
+        return TypesProviderPlugin;
+    }();
+    return __toCommonJS(src_exports);
+}();
 //# sourceMappingURL=index.global.js.map
