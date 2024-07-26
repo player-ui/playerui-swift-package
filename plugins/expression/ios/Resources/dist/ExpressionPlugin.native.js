7df8568af5fa6ccb8ec3b4759ec1cd6f3baefa6f
Intuit Service <opensource-svc@intuit.com>
2024-07-26 07:28:56 -0700

    Version 0.7.5--canary.443.15842

--- plugins/expression/ios/Resources/dist/ExpressionPlugin.native.js
+++ plugins/expression/ios/Resources/dist/ExpressionPlugin.native.js
@@ -1,41 +1,98 @@
 "use strict";
-var ExpressionPlugin = (() => {
-  var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
-  var __export = (target, all) => {
-    for (var name in all)
-      __defProp(target, name, { get: all[name], enumerable: true });
-  };
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+function _class_call_check(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
     }
-    return to;
-  };
-  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-  // ../../../../../../../../../../../execroot/_main/bazel-out/darwin_arm64-fastbuild/bin/plugins/expression/core/src/index.ts
-  var src_exports = {};
-  __export(src_exports, {
-    ExpressionPlugin: () => ExpressionPlugin
-  });
-  var ExpressionPlugin = class {
-    constructor(expressionMap) {
-      this.name = "ExpressionPlugin";
-      this.expressions = expressionMap;
+}
+function _defineProperties(target, props) {
+    for(var i = 0; i < props.length; i++){
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
     }
-    apply(player) {
-      player.hooks.expressionEvaluator.tap(this.name, (expEvaluator) => {
-        this.expressions.forEach((handler, name) => {
-          expEvaluator.addExpressionFunction(name, handler);
+}
+function _create_class(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+}
+var ExpressionPlugin = function() {
+    var __defProp = Object.defineProperty;
+    var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+    var __getOwnPropNames = Object.getOwnPropertyNames;
+    var __hasOwnProp = Object.prototype.hasOwnProperty;
+    var __export = function(target, all) {
+        for(var name in all)__defProp(target, name, {
+            get: all[name],
+            enumerable: true
         });
-      });
-    }
-  };
-  return __toCommonJS(src_exports);
-})();
+    };
+    var __copyProps = function(to, from, except, desc) {
+        if (from && typeof from === "object" || typeof from === "function") {
+            var _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
+            try {
+                var _loop = function() {
+                    var key = _step.value;
+                    if (!__hasOwnProp.call(to, key) && key !== except) __defProp(to, key, {
+                        get: function() {
+                            return from[key];
+                        },
+                        enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable
+                    });
+                };
+                for(var _iterator = __getOwnPropNames(from)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true)_loop();
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally{
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return != null) {
+                        _iterator.return();
+                    }
+                } finally{
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+        }
+        return to;
+    };
+    var __toCommonJS = function(mod) {
+        return __copyProps(__defProp({}, "__esModule", {
+            value: true
+        }), mod);
+    };
+    // ../../../../../../../../../../../execroot/_main/bazel-out/k8-fastbuild/bin/plugins/expression/core/src/index.ts
+    var src_exports = {};
+    __export(src_exports, {
+        ExpressionPlugin: function() {
+            return ExpressionPlugin;
+        }
+    });
+    var ExpressionPlugin = /*#__PURE__*/ function() {
+        function ExpressionPlugin(expressionMap) {
+            _class_call_check(this, ExpressionPlugin);
+            this.name = "ExpressionPlugin";
+            this.expressions = expressionMap;
+        }
+        _create_class(ExpressionPlugin, [
+            {
+                key: "apply",
+                value: function apply(player) {
+                    var _this = this;
+                    player.hooks.expressionEvaluator.tap(this.name, function(expEvaluator) {
+                        _this.expressions.forEach(function(handler, name) {
+                            expEvaluator.addExpressionFunction(name, handler);
+                        });
+                    });
+                }
+            }
+        ]);
+        return ExpressionPlugin;
+    }();
+    return __toCommonJS(src_exports);
+}();
 //# sourceMappingURL=index.global.js.map
