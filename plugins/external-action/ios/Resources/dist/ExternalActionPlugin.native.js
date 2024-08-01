91a345aa72db5ac2b7e1fca760e523b90c7463b8
Intuit Service <opensource-svc@intuit.com>
2024-07-31 22:58:10 -0700

    Version 0.8.0--canary.410.16189

--- plugins/external-action/ios/Resources/dist/ExternalActionPlugin.native.js
+++ plugins/external-action/ios/Resources/dist/ExternalActionPlugin.native.js
@@ -1,71 +1,290 @@
 "use strict";
-var ExternalActionPlugin = (() => {
-  var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
-  var __export = (target, all) => {
-    for (var name in all)
-      __defProp(target, name, { get: all[name], enumerable: true });
-  };
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+    try {
+        var info = gen[key](arg);
+        var value = info.value;
+    } catch (error) {
+        reject(error);
+        return;
     }
-    return to;
-  };
-  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-  // ../../../../../../../../../../../execroot/_main/bazel-out/darwin_arm64-fastbuild/bin/plugins/external-action/core/src/index.ts
-  var src_exports = {};
-  __export(src_exports, {
-    ExternalActionPlugin: () => ExternalActionPlugin
-  });
-  var ExternalActionPlugin = class {
-    constructor(handler) {
-      this.name = "ExternalActionPlugin";
-      this.handler = handler;
+    if (info.done) {
+        resolve(value);
+    } else {
+        Promise.resolve(value).then(_next, _throw);
     }
-    apply(player) {
-      player.hooks.flowController.tap(this.name, (flowController) => {
-        flowController.hooks.flow.tap(this.name, (flow) => {
-          flow.hooks.transition.tap(this.name, (fromState, toState) => {
-            const { value: state } = toState;
-            if (state.state_type === "EXTERNAL") {
-              setTimeout(async () => {
-                const shouldTransition = (currentState2) => currentState2.status === "in-progress" && currentState2.controllers.flow.current?.currentState?.value === state;
-                const currentState = player.getState();
-                if (shouldTransition(currentState)) {
-                  try {
-                    const transitionValue = await this.handler(
-                      state,
-                      currentState.controllers
-                    );
-                    if (transitionValue !== void 0) {
-                      const latestState = player.getState();
-                      if (shouldTransition(latestState)) {
-                        latestState.controllers.flow.transition(transitionValue);
-                      } else {
-                        player.logger.warn(
-                          `External state resolved with [${transitionValue}], but Player already navigated away from [${toState.name}]`
-                        );
-                      }
-                    }
-                  } catch (error) {
-                    if (error instanceof Error) {
-                      currentState.fail(error);
-                    }
-                  }
-                }
-              }, 0);
+}
+function _async_to_generator(fn) {
+    return function() {
+        var self = this, args = arguments;
+        return new Promise(function(resolve, reject) {
+            var gen = fn.apply(self, args);
+            function _next(value) {
+                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
             }
-          });
+            function _throw(err) {
+                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+            }
+            _next(undefined);
         });
-      });
+    };
+}
+function _class_call_check(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
     }
-  };
-  return __toCommonJS(src_exports);
-})();
+}
+function _defineProperties(target, props) {
+    for(var i = 0; i < props.length; i++){
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+    }
+}
+function _create_class(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+}
+function _instanceof(left, right) {
+    if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) {
+        return !!right[Symbol.hasInstance](left);
+    } else {
+        return left instanceof right;
+    }
+}
+function _ts_generator(thisArg, body) {
+    var f, y, t, g, _ = {
+        label: 0,
+        sent: function() {
+            if (t[0] & 1) throw t[1];
+            return t[1];
+        },
+        trys: [],
+        ops: []
+    };
+    return g = {
+        next: verb(0),
+        "throw": verb(1),
+        "return": verb(2)
+    }, typeof Symbol === "function" && (g[Symbol.iterator] = function() {
+        return this;
+    }), g;
+    function verb(n) {
+        return function(v) {
+            return step([
+                n,
+                v
+            ]);
+        };
+    }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while(_)try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [
+                op[0] & 2,
+                t.value
+            ];
+            switch(op[0]){
+                case 0:
+                case 1:
+                    t = op;
+                    break;
+                case 4:
+                    _.label++;
+                    return {
+                        value: op[1],
+                        done: false
+                    };
+                case 5:
+                    _.label++;
+                    y = op[1];
+                    op = [
+                        0
+                    ];
+                    continue;
+                case 7:
+                    op = _.ops.pop();
+                    _.trys.pop();
+                    continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+                        _ = 0;
+                        continue;
+                    }
+                    if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+                        _.label = op[1];
+                        break;
+                    }
+                    if (op[0] === 6 && _.label < t[1]) {
+                        _.label = t[1];
+                        t = op;
+                        break;
+                    }
+                    if (t && _.label < t[2]) {
+                        _.label = t[2];
+                        _.ops.push(op);
+                        break;
+                    }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop();
+                    continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) {
+            op = [
+                6,
+                e
+            ];
+            y = 0;
+        } finally{
+            f = t = 0;
+        }
+        if (op[0] & 5) throw op[1];
+        return {
+            value: op[0] ? op[1] : void 0,
+            done: true
+        };
+    }
+}
+var ExternalActionPlugin = function() {
+    var __defProp = Object.defineProperty;
+    var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+    var __getOwnPropNames = Object.getOwnPropertyNames;
+    var __hasOwnProp = Object.prototype.hasOwnProperty;
+    var __export = function(target, all) {
+        for(var name in all)__defProp(target, name, {
+            get: all[name],
+            enumerable: true
+        });
+    };
+    var __copyProps = function(to, from, except, desc) {
+        if (from && typeof from === "object" || typeof from === "function") {
+            var _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
+            try {
+                var _loop = function() {
+                    var key = _step.value;
+                    if (!__hasOwnProp.call(to, key) && key !== except) __defProp(to, key, {
+                        get: function() {
+                            return from[key];
+                        },
+                        enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable
+                    });
+                };
+                for(var _iterator = __getOwnPropNames(from)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true)_loop();
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally{
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return != null) {
+                        _iterator.return();
+                    }
+                } finally{
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+        }
+        return to;
+    };
+    var __toCommonJS = function(mod) {
+        return __copyProps(__defProp({}, "__esModule", {
+            value: true
+        }), mod);
+    };
+    // ../../../../../../../../../../../execroot/_main/bazel-out/k8-fastbuild/bin/plugins/external-action/core/src/index.ts
+    var src_exports = {};
+    __export(src_exports, {
+        ExternalActionPlugin: function() {
+            return ExternalActionPlugin;
+        }
+    });
+    var ExternalActionPlugin = /*#__PURE__*/ function() {
+        function ExternalActionPlugin(handler) {
+            _class_call_check(this, ExternalActionPlugin);
+            this.name = "ExternalActionPlugin";
+            this.handler = handler;
+        }
+        _create_class(ExternalActionPlugin, [
+            {
+                key: "apply",
+                value: function apply(player) {
+                    var _this = this;
+                    player.hooks.flowController.tap(this.name, function(flowController) {
+                        flowController.hooks.flow.tap(_this.name, function(flow) {
+                            flow.hooks.transition.tap(_this.name, function(fromState, toState) {
+                                var state = toState.value;
+                                if (state.state_type === "EXTERNAL") {
+                                    var _this1 = _this;
+                                    setTimeout(/*#__PURE__*/ _async_to_generator(function() {
+                                        var shouldTransition, currentState, transitionValue, latestState, error;
+                                        return _ts_generator(this, function(_state) {
+                                            switch(_state.label){
+                                                case 0:
+                                                    shouldTransition = function(currentState2) {
+                                                        var _currentState2_controllers_flow_current_currentState, _currentState2_controllers_flow_current;
+                                                        return currentState2.status === "in-progress" && ((_currentState2_controllers_flow_current = currentState2.controllers.flow.current) === null || _currentState2_controllers_flow_current === void 0 ? void 0 : (_currentState2_controllers_flow_current_currentState = _currentState2_controllers_flow_current.currentState) === null || _currentState2_controllers_flow_current_currentState === void 0 ? void 0 : _currentState2_controllers_flow_current_currentState.value) === state;
+                                                    };
+                                                    currentState = player.getState();
+                                                    if (!shouldTransition(currentState)) return [
+                                                        3,
+                                                        4
+                                                    ];
+                                                    _state.label = 1;
+                                                case 1:
+                                                    _state.trys.push([
+                                                        1,
+                                                        3,
+                                                        ,
+                                                        4
+                                                    ]);
+                                                    return [
+                                                        4,
+                                                        _this1.handler(state, currentState.controllers)
+                                                    ];
+                                                case 2:
+                                                    transitionValue = _state.sent();
+                                                    if (transitionValue !== void 0) {
+                                                        latestState = player.getState();
+                                                        if (shouldTransition(latestState)) {
+                                                            latestState.controllers.flow.transition(transitionValue);
+                                                        } else {
+                                                            player.logger.warn("External state resolved with [".concat(transitionValue, "], but Player already navigated away from [").concat(toState.name, "]"));
+                                                        }
+                                                    }
+                                                    return [
+                                                        3,
+                                                        4
+                                                    ];
+                                                case 3:
+                                                    error = _state.sent();
+                                                    if (_instanceof(error, Error)) {
+                                                        currentState.fail(error);
+                                                    }
+                                                    return [
+                                                        3,
+                                                        4
+                                                    ];
+                                                case 4:
+                                                    return [
+                                                        2
+                                                    ];
+                                            }
+                                        });
+                                    }), 0);
+                                }
+                            });
+                        });
+                    });
+                }
+            }
+        ]);
+        return ExternalActionPlugin;
+    }();
+    return __toCommonJS(src_exports);
+}();
 //# sourceMappingURL=index.global.js.map
