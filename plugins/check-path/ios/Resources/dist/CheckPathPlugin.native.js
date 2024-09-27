28ca36ec6d35a6888a866e5a1183371ce0e1f3d0
Intuit Service <opensource-svc@intuit.com>
2024-09-26 17:21:07 -0700

    Version 0.9.1

--- plugins/check-path/ios/Resources/dist/CheckPathPlugin.native.js
+++ plugins/check-path/ios/Resources/dist/CheckPathPlugin.native.js
@@ -4425,15 +4425,6 @@ var CheckPathPlugin = function() {
         ]);
         return Parser;
     }();
-    function unpackAndPush(item, initial) {
-        if (Array.isArray(item)) {
-            item.forEach(function(i) {
-                unpackAndPush(i, initial);
-            });
-        } else {
-            initial.push(item);
-        }
-    }
     var withContext = function(model) {
         return {
             get: function(binding, options) {
@@ -4654,14 +4645,7 @@ var CheckPathPlugin = function() {
                         var newValues = resolvedAST.values.map(function(mValue) {
                             var mTree = _this.computeTree(mValue, rawParentToPassIn, dataChanges, cacheUpdate, resolveOptions, resolvedAST, prevASTMap);
                             if (mTree.value !== void 0 && mTree.value !== null) {
-                                var _mTree_node_parent;
-                                if (((_mTree_node_parent = mTree.node.parent) === null || _mTree_node_parent === void 0 ? void 0 : _mTree_node_parent.type) === "multi-node" && Array.isArray(mTree.value)) {
-                                    mTree.value.forEach(function(v) {
-                                        unpackAndPush(v, childValue);
-                                    });
-                                } else {
-                                    childValue.push(mTree.value);
-                                }
+                                childValue.push(mTree.value);
                             }
                             mTree.dependencies.forEach(function(bindingDep) {
                                 return childDependencies.add(bindingDep);
