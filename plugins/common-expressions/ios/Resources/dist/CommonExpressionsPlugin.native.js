d3136758c7ef11c59c3fa92af0da8b4388c17605
Intuit Service <opensource-svc@intuit.com>
2024-12-04 08:58:00 -0800

    Version 0.10.0

--- plugins/common-expressions/ios/Resources/dist/CommonExpressionsPlugin.native.js
+++ plugins/common-expressions/ios/Resources/dist/CommonExpressionsPlugin.native.js
@@ -2789,6 +2789,8 @@ var CommonExpressionsPlugin = function() {
                 value += ch;
             }
             if (value) {
+                var maybeNumber = Number(value);
+                value = isNaN(maybeNumber) ? value : maybeNumber;
                 return toValue(value);
             }
         };
@@ -2897,6 +2899,10 @@ var CommonExpressionsPlugin = function() {
             if (firstSegment) {
                 parsed.push(firstSegment);
                 var bracketSegment = parseBracket();
+                if ((bracketSegment === null || bracketSegment === void 0 ? void 0 : bracketSegment.name) === "Value") {
+                    var maybeNumber = Number(bracketSegment.value);
+                    bracketSegment.value = isNaN(maybeNumber) || String(maybeNumber) !== bracketSegment.value ? bracketSegment.value : maybeNumber;
+                }
                 while(bracketSegment !== void 0){
                     parsed.push(bracketSegment);
                     bracketSegment = parseBracket();
