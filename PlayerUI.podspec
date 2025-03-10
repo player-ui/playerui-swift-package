ba95b651008579bae2bb894dcbd0de261d9b328e
Intuit Service <opensource-svc@intuit.com>
2025-03-10 10:30:14 -0700

    Version 0.11.0--canary.614.21244

--- PlayerUI.podspec
+++ PlayerUI.podspec
@@ -7,7 +7,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PlayerUI'
-  s.version          = '0.10.4'
+  s.version          = '0.11.0--canary.614.21244'
   s.summary          = 'A native renderer for Player content'
   s.swift_versions   = ['5.1']
   s.description      = <<-DESC
--- ios/core/Resources/dist/Player.native.js
+++ ios/core/Resources/dist/Player.native.js
@@ -5224,6 +5224,7 @@ var Player = function() {
         return _Builder;
     }();
     // ../../../../../../../../../../execroot/_main/bazel-out/k8-fastbuild/bin/core/player/src/view/plugins/template.ts
+    var templateSymbol = Symbol("template");
     var TemplatePlugin = /*#__PURE__*/ function() {
         function TemplatePlugin(options) {
             _class_call_check(this, TemplatePlugin);
@@ -5294,6 +5295,9 @@ var Player = function() {
                         override: false,
                         values: values
                     };
+                    if (node.placement !== void 0) {
+                        result[templateSymbol] = node.placement;
+                    }
                     return result;
                 }
             },
@@ -5307,6 +5311,38 @@ var Player = function() {
                         }
                         return node;
                     });
+                    function getTemplateSymbolValue(node) {
+                        if (node.type === "multi-node" /* MultiNode */ ) {
+                            return node[templateSymbol];
+                        } else if (node.type === "template" /* Template */ ) {
+                            return node.placement;
+                        }
+                        return void 0;
+                    }
+                    parser.hooks.onCreateASTNode.tap("template-sort", function(node) {
+                        if (node && (node.type === "view" /* View */  || node.type === "asset" /* Asset */ ) && Array.isArray(node.children)) {
+                            node.children = node.children.sort(function(a, b) {
+                                var pathsEqual = a.path.join() === b.path.join();
+                                if (pathsEqual) {
+                                    var aPlacement = getTemplateSymbolValue(a.value);
+                                    var bPlacement = getTemplateSymbolValue(b.value);
+                                    if (aPlacement !== void 0 && bPlacement === void 0) {
+                                        return aPlacement === "prepend" ? -1 : 1;
+                                    } else if (bPlacement !== void 0 && aPlacement === void 0) {
+                                        return bPlacement === "prepend" ? 1 : -1;
+                                    } else if (aPlacement !== void 0 && bPlacement !== void 0) {
+                                        if (aPlacement === bPlacement) {
+                                            return 0;
+                                        }
+                                        return aPlacement === "prepend" ? -1 : 1;
+                                    }
+                                    return 0;
+                                }
+                                return 0;
+                            });
+                        }
+                        return node;
+                    });
                     parser.hooks.parseNode.tap("template", function(obj, _nodeType, options, childOptions) {
                         if (childOptions && hasTemplateKey(childOptions.key)) {
                             return obj.map(function(template) {
@@ -5316,7 +5352,8 @@ var Player = function() {
                                     depth: (_options_templateDepth = options.templateDepth) !== null && _options_templateDepth !== void 0 ? _options_templateDepth : 0,
                                     data: template.data,
                                     template: template.value,
-                                    dynamic: (_template_dynamic = template.dynamic) !== null && _template_dynamic !== void 0 ? _template_dynamic : false
+                                    dynamic: (_template_dynamic = template.dynamic) !== null && _template_dynamic !== void 0 ? _template_dynamic : false,
+                                    placement: template.placement
                                 }, template);
                                 if (!templateAST) return;
                                 if (templateAST.type === "multi-node" /* MultiNode */ ) {
@@ -5352,6 +5389,7 @@ var Player = function() {
                 value: function apply(view) {
                     view.hooks.parser.tap("template", this.applyParser.bind(this));
                     view.hooks.resolver.tap("template", this.applyResolverHooks.bind(this));
+                    view.hooks.onTemplatePluginCreated.call(this);
                 }
             }
         ]);
