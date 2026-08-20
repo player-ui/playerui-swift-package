9e1ad5135fe1a8f4161c7ac595ea24598ba4cc7e
Intuit Service <opensource-svc@intuit.com>
2026-08-20 02:43:14 -0700

    Version 1.1.1--canary.937.41794

--- ios/core/Sources/Types/Core/PlayerView.swift
+++ ios/core/Sources/Types/Core/PlayerView.swift
@@ -1,6 +1,6 @@
 //
-//  PlayerView.swift
-//
+//  File.swift
+//  
 //
 //  Created by Borawski, Harris on 2/12/20.
 //
@@ -8,37 +8,41 @@
 import Foundation
 import JavaScriptCore
 
-/// A wrapper around the JS View in the core player
+/**
+A wrapper around the JS View in the core player
+*/
 public class PlayerView: CreatedFromJSValue {
+    /// Typealias for associated type
+    public typealias T = PlayerView
+
+    /**
+    Creates an instance from a JSValue, used for generic construction
+    - parameters:
+       - value: The JSValue to construct from
+    */
+    public static func createInstance(value: JSValue) -> PlayerView { PlayerView(value) }
+
+    /// The JSValue that backs this wrapper
+    internal let value: JSValue
+
     /// The hooks that can be tapped into
     public let hooks: ViewHooks
 
-    /// The JSValue that backs this wrapper
-    let value: JSValue
+    /**
+    Construct a View from a JSValue
+    - parameters:
+       - value: The JSValue that is the View
+    */
+    public init(_ value: JSValue) {
+        self.value = value
+        self.hooks = ViewHooks(onUpdate: Hook<JSValue>(baseValue: self.value, name: "onUpdate"))
+    }
 
     /// Gets the ID for this view
     public var id: String {
-        value
+        return value
             .objectForKeyedSubscript("initialView")?
             .objectForKeyedSubscript("id")?
             .toString() ?? "something went really wrong and this view has no id"
     }
-
-    /// Construct a View from a JSValue
-    /// - parameters:
-    ///   - value: The JSValue that is the View
-    public init(_ value: JSValue) {
-        self.value = value
-        hooks = ViewHooks(onUpdate: Hook<JSValue>(baseValue: self.value, name: "onUpdate"))
-    }
-
-    /// Creates an instance from a JSValue, used for generic construction
-    /// - parameters:
-    ///   - value: The JSValue to construct from
-    public static func createInstance(value: JSValue) -> PlayerView {
-        PlayerView(value)
-    }
-
-    /// Typealias for associated type
-    public typealias T = PlayerView
 }
