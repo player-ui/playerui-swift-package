ea6b48dcaa67e9d65bee8998dba74b356b116e17
Intuit Service <opensource-svc@intuit.com>
2026-09-01 11:13:13 -0700

    Version 1.2.0--canary.934.42511

--- ios/core/Sources/utilities/ResourceUtilities.swift
+++ ios/core/Sources/utilities/ResourceUtilities.swift
@@ -7,28 +7,22 @@
 
 import Foundation
 
-/**
- Utilities for loading resource files
- */
+/// Utilities for loading resource files
 public class ResourceUtilities {
-    /**
-     Gets the URL for a file in the bundle
-     Example:
-     ```
-     ResourceUtilities.urlForFile(name: "plugin", ext: "js", bundle: Bundle(for: SomeClass.self), pathComponent: "MyPodName.bundle")
-     ```
-
-     - parameters:
-        - name: The name of the file
-        - ext: The extension of the file
-        - bundle: The bundle to load URLs from
-        - pathComponent: The string bundle path to append to the bundle resource URL
-     - returns:
-        A URL to the file if the bundle can be loaded
-     */
-    public static func urlForFile(name: String, ext: String, bundle: Bundle, pathComponent: String? = nil) -> URL? {
-        let baseURL = pathComponent.map { bundle.resourceURL?.appendingPathComponent($0) } ?? bundle.resourceURL
-        guard let bundleURL = baseURL else { return nil }
+    /// Gets the URL for a file in the bundle
+    /// Example:
+    /// ```
+    /// ResourceUtilities.urlForFile(name: "plugin", ext: "js", bundle: Bundle.module)
+    /// ```
+    ///
+    /// - parameters:
+    ///   - name: The name of the file
+    ///   - ext: The extension of the file
+    ///   - bundle: The bundle to load URLs from
+    /// - returns:
+    ///   A URL to the file if the bundle can be loaded
+    public static func urlForFile(name: String, ext: String, bundle: Bundle) -> URL? {
+        guard let bundleURL = bundle.resourceURL else { return nil }
         return Bundle(url: bundleURL)?.url(forResource: name, withExtension: ext)
     }
 }
