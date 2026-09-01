f74cbd4e37a059faf1d2214e1f1112e8cefbf7cc
Intuit Service <opensource-svc@intuit.com>
2026-09-01 09:23:36 -0700

    Version 1.2.0-next.5

--- ios/swiftui/Sources/ManagedPlayer/ScrollPlugin.swift
+++ ios/swiftui/Sources/ManagedPlayer/ScrollPlugin.swift
@@ -1,8 +1,5 @@
-import SwiftUI
-
-#if SWIFT_PACKAGE
 import PlayerUI
-#endif
+import SwiftUI
 
 /// A plugin to wrap player content in a scrollview
 /// and provide `EnvironmentValues` for
@@ -16,14 +13,14 @@ public class ScrollPlugin: NativePlugin {
     /// Wraps SwiftUIPlayer content in `ScrollView`
     /// - Parameter edgesIgnoringSafeArea: Edges to ignore for the safe area
     public init(edgesIgnoringSafeArea: Edge.Set = []) {
-        self.ignoredEdges = edgesIgnoringSafeArea
+        ignoredEdges = edgesIgnoringSafeArea
     }
 
-    public func apply<P>(player: P) where P: HeadlessPlayer {
+    public func apply(player: some HeadlessPlayer) {
         guard let player = player as? SwiftUIPlayer else { return }
-        let ignoredEdges = self.ignoredEdges
+        let ignoredEdges = ignoredEdges
         player.hooks?.view.tap(name: pluginName) { view in
-            return AnyView(
+            AnyView(
                 ScrollViewReader { proxy in
                     ScrollView {
                         view.scrollToProxy(proxy)
@@ -38,7 +35,9 @@ public class ScrollPlugin: NativePlugin {
 
 struct MeasureToEnvironment: ViewModifier {
     let path: WritableKeyPath<EnvironmentValues, CGSize>
+
     @State var size: CGSize = .zero
+
     func body(content: Content) -> some View {
         content
             .background(
@@ -46,7 +45,6 @@ struct MeasureToEnvironment: ViewModifier {
                     Color.clear
                         .onAppear { size = proxy.size }
                         .onChange(of: proxy.size, perform: { size = $0 })
-
                 }
             )
             .environment(path, size)
