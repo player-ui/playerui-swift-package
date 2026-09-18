abc6ee472b3882a45d7faad62df3dfbc448ae7b0
Intuit Service <opensource-svc@intuit.com>
2026-09-18 13:53:31 -0700

    Version 1.2.1--canary.952.42743

--- ios/core/Resources/dist/Player.native.js
+++ ios/core/Resources/dist/Player.native.js
@@ -8505,8 +8505,8 @@ var Player = function() {
         status: "not-started"
     };
     // ../../../../../../../../../../execroot/_main/bazel-out/k8-fastbuild/bin/core/player/src/player.ts
-    var PLAYER_VERSION = true ? "1.2.0" : "unknown";
-    var COMMIT = true ? "d27e6cf75db1cde6b8f84bd84215bcddc7e8d5e6" : "unknown";
+    var PLAYER_VERSION = true ? "1.2.1--canary.952.42743" : "unknown";
+    var COMMIT = true ? "0359432e9ad5b55adefbcbb84e61f9f1f556725e" : "unknown";
     var _Player = /*#__PURE__*/ function() {
         function _Player(config) {
             var _this = this;
--- plugins/external-state/ios/Resources/dist/ExternalStatePlugin.native.js
+++ plugins/external-state/ios/Resources/dist/ExternalStatePlugin.native.js
@@ -7850,8 +7850,8 @@ var ExternalStatePlugin = function() {
         ref: Symbol("not-started"),
         status: "not-started"
     };
-    var PLAYER_VERSION = true ? "1.2.0" : "unknown";
-    var COMMIT = true ? "d27e6cf75db1cde6b8f84bd84215bcddc7e8d5e6" : "unknown";
+    var PLAYER_VERSION = true ? "1.2.1--canary.952.42743" : "unknown";
+    var COMMIT = true ? "0359432e9ad5b55adefbcbb84e61f9f1f556725e" : "unknown";
     var _Player = /*#__PURE__*/ function() {
         function _Player2(config) {
             var _this = this;
